2330522e7d93b64b79f62a442fd73b6fd3fec3fe
ferris <yupferris@gmail.com>
2016-12-07 21:31:54 +0100

    Playing around with test interfaces
    Already seeing I need to hook something real up to this before fleshing this stuff out too much

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+const MEMORY_SIZE: usize = 65536;
+type Memory<'a> = &'a [u8; MEMORY_SIZE];
+
+#[derive(Debug)]
+struct Status {
+    half_cycle: usize,
+    phi_0: bool,
+    address_bus: u16,
+    data_bus: u8,
+    read_write: bool,
+    pc: u16,
+    a: u8,
+    x: u8,
+    y: u8,
+    sp: u8,
+    p: u8,
+    ir: u8
+}
+
+trait TestModule {
+    fn step(&mut self);
+    fn status(&self) -> Status;
+
+    fn memory(&self) -> &Memory;
+    fn set_memory(&mut self, mem: &Memory);
+}
+
 fn main() {
     println!("Hello, world!");
 }
